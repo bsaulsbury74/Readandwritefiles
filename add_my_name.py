07be7bafaf64959c636f97d4b935393220e53233
Brendan <brendasa7@gmail.com>
2022-09-01 13:09:48 -0500

    Changes to Ch six

---
+++ add_my_name.py
@@ -0,0 +1,8 @@
+def main():
+    outfile = open('philosophers.txt','a')
+
+    outfile.write('Brendan Saulsbury\n')
+
+    outfile.close()
+
+main()
---
+++ print_customers.py
@@ -0,0 +1,18 @@
+import csv
+
+infile= open('customers.csv','r')
+
+csvfile= csv.reader(infile, delimiter=',')
+
+next(csvfile)               #this will skip first row
+
+for record in csvfile:
+    print("ID:",record[0])
+    print("First Name:",record[1])
+    print("Last Name:",record[2])
+    print("City:",record[3])
+    print("Country:",record[4])
+    print("Phone:",record[5])
+    input()
+
+
